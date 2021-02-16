c32143bcf90416e9d5ea0c5f1c97183aeb3e1663
JanekBancarewicz <jan.bancarkiewicz@rb.com>
2021-02-16 10:37:05 +0100

    from net config

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -8,7 +8,16 @@ const NewsLetter = () => {
           <h2>Get all the latest stories to your inbox</h2>
           <h4>i write to my friends every few weeks</h4>
 
-          <form className="contact-form">
+          <form
+            className="contact-form"
+            name="testing-contact"
+            method="post"
+            netlify-honeypot="bot-field"
+            data-netlify="true"
+            action="/success"
+          >
+            <input type="hidden" name="bot-field" />
+            <input type="hidden" name="form-name" value="testing-contact" />
             <input
               type="text"
               name="name"
